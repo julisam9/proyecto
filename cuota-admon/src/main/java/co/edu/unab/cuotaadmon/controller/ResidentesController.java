54dd0232224ee17679b1b07884b337c880bc4230
julisam9 <julisam9@gmail.com>
2022-10-27 22:41:24 -0500

    Backend CRUD completo

--- cuota-admon/src/main/java/co/edu/unab/cuotaadmon/controller/ResidentesController.java
+++ cuota-admon/src/main/java/co/edu/unab/cuotaadmon/controller/ResidentesController.java
@@ -4,17 +4,23 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
 import co.edu.unab.cuotaadmon.model.ResidentesModel;
 import co.edu.unab.cuotaadmon.service.ResidentesServices;
 
 @RestController
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE })
 @RequestMapping(path = "/residentes")
 public class ResidentesController {
     @Autowired
@@ -37,6 +43,12 @@ public class ResidentesController {
     @PostMapping(path = "/guardar")
     public String guardarResidente(@RequestBody ResidentesModel residente) {
         return residentesServices.saveResidentes(residente);
+
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String borrarResidente(@PathVariable("id") int id) {
+        return residentesServices.deleteResidentes(id);
 
     }
 
